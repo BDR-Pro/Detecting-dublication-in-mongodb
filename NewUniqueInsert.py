bdff218467136bac838f81fa0e26ccb3b97b415d
BDR-Pro <baderalotaibi3@gmail.com>
2023-05-04 21:47:58 +0300

    Update NewUniqueInsert.py

--- NewUniqueInsert.py
+++ NewUniqueInsert.py
@@ -1,17 +1,23 @@
 
-def UniqueInsert(collection,list_,index,value):        #function to insert the data
-    if(inmongo(collection,list_,index,value)==False) :
+
+def UniqueInsert(collection, list_, index):
+    if not inmongo(collection, list_, index):
         try:
             collection.insert_one(list_)
+            print(f"inserted {list_}")
         except Exception:
-            collection = db[list_["Series"]] #create collection if not exist
-            UniqueInsert(collection,list_,index,value) #insert the data
+            collection = db[list_["Series"]]
+            UniqueInsert(collection, list_)
         return True
-    return False    
-    
-def inmongo(collection,list_,index,value):             #function to check if the data is already in the database
+    return False
+
+
+def inmongo(collection, list_, index):
     try:
-        search=collection.find_one({index: list_[value]})
+        search = collection.find_one({index: list_[index]})
     except:
-        return search is None
-    return search
+        return False
+    if search is not None:
+        print(f"found {search}")
+        return True
+    return False
